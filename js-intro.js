dc7b183cb6ab4c2b851ce54299affb38443b8e52
Ubuntu <ubuntu@ip-172-31-39-185.us-east-2.compute.internal>
2019-08-07 00:07:42 +0000

    completed assessment

--- js-intro.js
+++ js-intro.js
@@ -4,14 +4,15 @@
 // ---------------------------------------------
 // Consider this variable:
 
+
 var mantra = "Be the dev"
 
 // 1a. Write the code that returns 'B' from mantra.
-
+console.log(mantra.charAt(0))
 // 1b. Write the code that determines if there is an 'x' in mantra.
-
+console.log(mantra.includes("x"))
 // 1c. Write the code that determines if there is a 'v' in mantra.
-
+console.log(mantra.includes("v"))
 // 1d. Stretch: Write a function that can take any letter as an argument and returns if it exists within the given string.
 
 
@@ -23,10 +24,29 @@ var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
 
+function length (dog, cat) {
+    if (dog.length > cat.length){
+        return dog
+    }else {
+        return cat
+    }
+}
+
+console.log(length(myDog, myCat));
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
-
+let pet = [myDog,myCat]
+console.log(pet)
 // 2c. Stretch: Write a function that returns the two variables in all lowercase letters.
-
+// function lower (dog1,cat1) {
+//     if(dog1 === "Snoopy"){
+//     return dog1.tolowerCase()
+// }else if (cat1 === "Garfield"){
+//     return cat1.tolowerCase()
+// }else{
+//     return "hi"
+// }
+// }
+// console.log(lower(myDog,myCat))
 
 // -----------------------------------------------
 // Consider this variable:
@@ -34,7 +54,10 @@ var myCat = "Garfield"
 var myMessage = "Learning is fun!"
 
 // 3a. Write the code that logs each letter of the message using a for loop.
-
+let seperate = myMessage.split("")
+console.log(seperate);
+for(i =0 ; i < seperate.length; i++)
+    console.log(seperate[i]);
 // 4b. Stretch: Write the code that logs each letter of the message using a while loop.
 
 // 4c. Super Stretch: Write the code that logs each letter of the message using forEach().
@@ -46,7 +69,20 @@ var myMessage = "Learning is fun!"
 var testString = "thisisateststring"
 
 // 5a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
+var  testString= "thisisateststring"
+let yup = testString.split("")
 
+function remo (sep){
+  for( let i =0 ; i < yup.length; i++)
+  const win = yup.length[i]
+  if(yup[i] === "a" || yup[i] === "e" || yup[i] ==="i" || yup[i] ===
+"o" ||yup[i] === "u"){
+return yup[i].join("")
+}
+}
+
+
+    console.log(remo(yup))
 // 5b. Stretch: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
 
 
@@ -56,7 +92,28 @@ var testString = "thisisateststring"
 var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
 
 // 6a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
-
+var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+cats =(...toonimals) => {
+  const {animal} = toonimals
+  for (let i = 0; i < toonimals.length; i++)
+var array = {}
+  if(animal === "cat"){
+    return array
+  }else{
+    return "hi"
+  }
+}
+console.log(cats(toonimals));
 //6b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
-
+var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
+cats =(...toonimals) => {
+  const {animal} = toonimals
+  for (let i = 0; i < toonimals.length; i++)
+  if(animal!=== "cat"){
+    return array
+  }else{
+    return "hi"
+  }
+}
+console.log(cats(toonimals));
 //6c. Stretch: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
--- js-intro.md
+++ js-intro.md
@@ -6,68 +6,84 @@ Answer the following questions. First, without external resources. Challenge you
 1. What is a function? Why would you use one?
 
   Your answer:
-
+the procedure
   Researched answer:
-
+A function is a subprogram designed to perform a particular task
 
 2. What is a method?
 
   Your answer:
-
+an action
   Researched answer:
-
+actions that can be performed on objects
 
 3. List 5 examples of JavaScript string methods.
 
   Your answer:
-
+splice(), split(), charAt(), toLowerCase(), toUpperCase()
   Researched answer:
-
+charAt()	Returns the character at the specified index (position)
+charCodeAt()	Returns the Unicode of the character at the specified index
+concat()	Joins two or more strings, and returns a new joined strings
+endsWith()	Checks whether a string ends with specified string/characters
+fromCharCode()	Converts Unicode values to characters
+includes()	Checks whether a string contains the specified string/characters
+indexOf()	Returns the position of the first found occurrence of a specified value in a string
 
 4. List 5 examples of JavaScript array methods.
 
   Your answer:
-
+concat(), join(),pop(), push()
   Researched answer:
-
+additionals were reverse(), split(), splice()
 
 5. Explain the difference between accessor and mutator methods. Provide an example of each.
 
   Your answer:
-
+ways to access data
   Researched answer:
+An accessor is a class method used to read data members, while a mutator is a class method used to change data members.
+class foo
+{
+    private:
 
+        int a;
+    public:
+        int  accessA() const { return(a);}
+        void mutateA(const int A) { a = A;}
 
 6. Explain the difference between console.log() and return.
 
   Your answer:
-
+console log  is a way for developers to check if their code is running.  return is the action inside of a function.
   Researched answer:
-
+console.log is only used for debugging/informative purposes. return is an integral part of the Javascript language
 
 7. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
   Your answer:
-
+The argument is varible or info that function will run through. A variable stores information.
   Researched answer:
-
+In simpler terms, the argument the actual value that is supplied to a function whereas the parameter is the variable inside the definition of the function
 
 8. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
   Your answer:
-
+It is a programming language and is the background proceess for almost everything you see on the internet.
   Researched answer:
-
+JavaScript is a multi-paradigm, dynamic language with types and operators, standard built-in objects, and methods
 
 9. Give a brief description of proper pair programming techniques. What are the rolls of each person?
 
   Your answer:
+The idea is that 2 heads are better than one.  There is a driver and navigator.  The driver types out the information that is relayed by navigator.
 
   Researched answer:
 
-
+Pair programming is an agile software development technique in which two programmers work together at one workstation. One, the driver, writes code while the other, the observer or navigator, reviews each line of code as it is typed in.
 10. What is one thing we did in class this week you found helpful?  
 
   Your answer:
-
+The encouragement from the instructors.
   Researched answer:
+N/A
